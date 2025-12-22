00a07bc07b47fdd9f8ffa176cdc8d8091fdafa69
tony <f0952747529@gmail.com>
2025-12-22 15:11:08 +0800

    新增 .\leetcode\A977_v1.c

---
+++ leetcode/A977_v1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* sortsquare(int* num, int size, int* returnsize){
+    int* result = (int *)malloc(sizeof(int) * size);
+    int i = 0;
+    int j = size - 1;
+    int k = size - 1;
+    while(i <= j){
+        int left = num[i];
+        int right = num[j];
+        int leftsq = left * left;
+        int rightsq = right * right;
+        if(leftsq < rightsq){
+            result[k--] = rightsq;
+            j--;
+        }else{
+            result[k--] = leftsq;
+            i++;
+        }
+    }
+    *returnsize = size; 
+    return result;    
+}
+int main(){
+    int num[] = {2, 4, 6, -1, 5};
+    int size = sizeof(num) / sizeof(num[0]);
+    int returnsize = 0; 
+    int* ans = sortsquare(num, size, &returnsize);
+
+    for(int i = 0; i < size; i++){
+        printf("%d ", ans[i]);
+    }
+    printf("\n");
+}
