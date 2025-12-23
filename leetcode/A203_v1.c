0786d0c7ec8f12db90012c9dcb2ae949c352384e
tony <f0952747529@gmail.com>
2025-12-23 10:39:21 +0800

    add ./leetcode/A203_v1.c

---
+++ leetcode/A203_v1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct listnode{
+    int data;
+    struct listnode *next;
+};
+
+void deletenode(int **head, int data){
+    struct listnode *prev = NULL, *curr = *head;
+    while(curr && curr -> data != data){
+        prev = curr;
+        curr -> next = curr;
+    }
+    if(curr == NULL){
+        printf("node doesn't exist!");
+    }else if(*head = curr){
+        *head = curr -> next;
+        free(curr);
+    }else{
+        prev -> next = curr -> next;
+        free(curr);
+    }
+}
