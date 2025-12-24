b4a4fc0726b4f317366e77c255645ee9fbf1675a
tony <f0952747529@gmail.com>
2025-12-24 11:18:36 +0800

    新增 .\leetcode\A24_v1.c

---
+++ leetcode/A24_v1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct listnode{
+    int data;
+    struct listnode *next;
+};
+
+// push node
+void push(struct listnode **head, int data){
+    struct listnode *new_node = NULL;
+    new_node = (struct listnode *)malloc(sizeof(struct listnode));
+    if(new_node == NULL) return;
+    new_node -> next = *head;
+    new_node -> data = data;
+    *head = new_node;
+}
+
+void swapPairs(struct listnode **head){
+    struct listnode *new_node = NULL;
+    new_node = (struct listnode *)malloc(sizeof(struct listnode));
+    new_node -> next = *head;
+    struct listnode *curr = new_node;
+    while(curr -> next && curr -> next -> next){
+        struct listnode *first = curr -> next;
+        struct listnode *second = curr -> next -> next;
+
+        first -> next = second -> next;
+        second -> next = first;
+        curr -> next = second;
+        curr = first;
+    }
+    *head = new_node -> next;
+    free(new_node);
+}
+
+// print list
+void printlist(struct listnode *head){
+    struct listnode *ptr = head;
+    while(ptr -> next){
+        printf("%d -> ", ptr -> data);
+        ptr = ptr -> next;
+    }
+    printf("%d\n", ptr -> data);
+}
+
+int main(){
+    struct listnode *head = NULL;
+    push(&head, 30);
+    push(&head, 40);
+    push(&head, 50);
+    push(&head, 60);
+    swapPairs(&head);
+    printlist(head);
+}
