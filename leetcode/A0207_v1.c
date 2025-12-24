cf7e00537b55e59a85106e7d030954e27ca61c42
tony <f0952747529@gmail.com>
2025-12-24 14:27:49 +0800

    ./leetcode/A0207_v1.c

---
+++ leetcode/A0207_v1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct listnode{
+    int data;
+    struct listnode *next;
+};
+
+struct listnode* getInsertionNode(struct listnode *headA, struct listnode *headB){
+    struct listnode *curA = headA;
+    struct listnode *curB = headB;
+    int lenA = 0, lenB = 0;
+    while(curA != NULL){
+        lenA++;
+        curA = curA -> next;
+    }
+    while(curB != NULL){
+        lenB++;
+        curB = curB -> next;
+    }
+
+    curA = headA;
+    curB = headB;
+    if(lenB > lenA){
+        int tmp = lenA;
+        lenA = lenB;
+        lenB = tmp;
+
+        struct listnode *tmpNode = curA;
+        curA = curB;
+        curB = tmpNode;
+    }
+    int gap = lenA - lenB;
+    while(gap--){
+        curA = curA -> next;
+    }
+    while(curA){
+        if(curA == curB){
+            return curA;
+        }
+        curA = curA -> next;
+        curB = curB -> next;
+    }
+    return NULL;
+}
